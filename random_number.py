ab927480aed4ab79763d1465a66a78c9d2051756
DawidSzoka1 <oficer130@gmail.com>
2023-03-31 12:51:54 +0200

    staring page

--- random_number.py
+++ random_number.py
@@ -1,2 +1,34 @@
-from flask import flash, request, render_template
+from flask import Flask, request
 
+app = Flask(__name__)
+
+
+
+
+
+
+
+@app.route("/", methods=["POST", "GET"])
+def guess():
+    start = """
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <title>I will guess your number</title>
+    </head>
+    <body>
+    <h1>Imagine number between 0 and 1000</h1>
+    <form action="" method="POST">
+        <input type="hidden" name="min" value="{}"></input>
+        <input type="hidden" name="max" value="{}"></input>
+        To start press button
+        <input type="submit" value="OK">
+    </form>
+    </body>
+    </html>
+    """
+
+
+if __name__ == "__main__":
+    app.run(debug=True)

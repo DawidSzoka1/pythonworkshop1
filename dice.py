c08c0f62e4f771ce81f6dce39d539ca53c20db53
DawidSzoka1 <oficer130@gmail.com>
2023-03-31 15:02:30 +0200

    final commit

--- dice.py
+++ dice.py
@@ -5,16 +5,16 @@ def dice(dice):
     posible_dices = ["D3", "D4", "D6", "D8", "D10", "D12", "D20", "D100"]
     modifier = ["+", "-"]
     thrown_number = 0
-    list_dice = [i for i in dice]
+    numbers_of_throw = 1
     no_modif = True
-    for i in range(len(list_dice)):  # checking if dice have modifierf if yes then spliting dice
-        if list_dice[i] == modifier[0]:
+    for i in range(len(dice)):  # checking if dice have modifierf if yes then spliting dice
+        if dice[i] == modifier[0]:
             split_dice = dice.split("+")
             throw = split_dice[0]
             modif = split_dice[1]
             modif_type = "+"
             no_modif = False
-        elif list_dice[i] == modifier[1]:
+        elif dice[i] == modifier[1]:
             split_dice = dice.split("-")
             throw = split_dice[0]
             modif = split_dice[1]
@@ -36,7 +36,11 @@ def dice(dice):
                     thrown_number += random.randint(1, int(dice[position_of_D + 1:]))
             else:
                 thrown_number += random.randint(1, int(dice[position_of_D + 1:]))
-            return thrown_number
+            strin = f"""
+Throwing dice type {dice[position_of_D:]}, 
+{numbers_of_throw} times without modifier you got: {thrown_number}            
+            """
+            return strin
     else:
         if dice[:position_of_D]:
             for i in range(numbers_of_throw):
@@ -47,17 +51,20 @@ def dice(dice):
                 thrown_number -= int(modif)
                             
         else:
+
             if modif_type == "+":
                 thrown_number += random.randint(1, int(throw[position_of_D + 1:]))
                 thrown_number += int(modif)
             else:
                 thrown_number += random.randint(1, int(throw[position_of_D + 1:]))
                 thrown_number -= int(modif)
-                
-        return thrown_number
+        strin = f"""
+Throwing dice type {throw[position_of_D:]}, 
+{numbers_of_throw} times with modifier {modif_type+modif} you got: {thrown_number}"""
+        return strin
 
 
 print(dice("2D10+10"))
 print(dice("D10"))
 print(dice("3D10"))
-print(dice("6D100-10"))
+print(dice("D100-10"))

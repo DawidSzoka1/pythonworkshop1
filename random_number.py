9999baee8df8c19f701fb77b7536c6e36ec2718f
DawidSzoka1 <oficer130@gmail.com>
2023-03-31 12:57:19 +0200

    return and guessing page

--- random_number.py
+++ random_number.py
@@ -10,7 +10,8 @@ app = Flask(__name__)
 
 @app.route("/", methods=["POST", "GET"])
 def guess():
-    start = """
+    #main page that you will see using method get
+    main_page = """
     <!DOCTYPE html>
     <html lang="en">
     <head>
@@ -28,7 +29,33 @@ def guess():
     </body>
     </html>
     """
+    gues = """
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+          <meta charset="UTF-8">
+          <title>Guess The Number</title>
+        </head>
+        <body>
+         <form action="/" method="POST">
+            <label>
+            <h1>Is it {guess}?</h1>
 
+            <INPUT TYPE="submit" name="choice" Value="small">
+            <INPUT TYPE="submit" name="choice" Value="big">
+            <INPUT TYPE="submit" name="choice" Value="win">
+            <input type="hidden" name="min" value="{min}">
+            <input type="hidden" name="max" value="{max}">
+            <input type="hidden" name="guess" value="{guess}">
+            </label>
+        </form>
+        </body>
+        </html>
+        """
+    if request.method == "GET":
+        return main_page.format(0, 1000)
+    else:
+        return gues
 
 if __name__ == "__main__":
     app.run(debug=True)

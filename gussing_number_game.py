14592d7836438896da3457e1c898693ebcb469a6
DawidSzoka1 <oficer130@gmail.com>
2023-03-31 13:31:51 +0200

    last commit

--- gussing_number_game.py
+++ gussing_number_game.py
@@ -5,12 +5,16 @@ user_guess = input("Guess the number: ")
 while True:
     try:
         int(user_guess)
-        break
+        if int(user_guess) < random_number:
+            print("Too small!")
+            user_guess = input("Guess the number: ")
+        elif int(user_guess) > random_number:
+            print("Too big!")
+            user_guess = input("Guess the number: ")
+        else:
+            print("You won!")
+            break
     except ValueError:
         user_guess = input("Its not a number try again: ")
     except Exception:
         user_guess = input("There was a problem please try again: ")
-
-
-def gussing(random, user):
-    pass
